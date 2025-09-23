98c4c8d6dcc8c5ee3268051fb639f24cd019cc9f
Amith B V <amithrao0606@gmail.com>
2025-09-23 18:06:57 +0530

    Added new interface and updated the tsconfig file of "user-app".

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,8 +1,15 @@
-export interface CustomUser {
+export interface GoogleUser {
   id?: string | null;
-  name?: string | null;
-  email?: string | null;
+  name: string;
+  email: string;
   image?: string | null;
-  provider?: string | null;
-  token?: string | null;
+}
+
+export interface CustomUser {
+  name: string;
+  email: string;
+  number?: string | null;
+  image?: string | null;
+  oauth_id: string;
+  provider: "Google";
 }
