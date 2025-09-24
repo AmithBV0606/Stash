b2dae254b8e1fff3b146633caafbb4187f2b37c9
Amith B V <amithrao0606@gmail.com>
2025-09-24 08:32:17 +0530

    Moved the contents of "app/page.tsx" to "app/home/page.tsx".

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,57 +1,10 @@
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@workspace/ui/components/alert-dialog";
-import { Button } from "@workspace/ui/components/button";
-import { prisma } from "@repo/db";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/options";
+import { HeroHeader } from "@/components/header";
+import React from "react";
 
-export default async function Page() {
-  const user = await prisma.user.findFirst();
-  const session = await getServerSession(authOptions);
-
-  console.log("Session :", session);
-
+export default function page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-
-        <div className="bg-orange-400 p-4 rounded-lg text-black">
-          <p className="font-bold">Name: {user?.name}</p>
-          <p className="font-bold">Email : {user?.email}</p>
-          <p className="font-bold">Ph No : {user?.number}</p>
-        </div>
-
-        <Button size="sm">Button</Button>
-
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button variant="outline">Show Dialog</Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      </div>
+    <div>
+      <HeroHeader />
     </div>
   );
 }
