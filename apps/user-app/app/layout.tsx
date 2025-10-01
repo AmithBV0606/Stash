61d946793dbfef5c53079d1292b02ef6fd0435d3
Amith B V <amithrao0606@gmail.com>
2025-10-01 22:49:12 +0530

    Added "react-toastify" package.

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,30 +1,44 @@
-import { Geist, Geist_Mono } from "next/font/google"
-
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
+import { Geist, Geist_Mono } from "next/font/google";
+import "@workspace/ui/globals.css";
+import { Providers } from "@/components/providers";
+import { ToastContainer } from "react-toastify";
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
