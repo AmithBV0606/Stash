39650014d98921d6570c26c6f918ddca7341bc94
Amith B V <amithrao0606@gmail.com>
2025-10-05 09:27:40 +0530

    Removed the sidebar component as it was causing memory overload.

--- apps/user-app/app/dashboard/page.tsx
+++ apps/user-app/app/dashboard/page.tsx
@@ -3,12 +3,6 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/options";
 import ClientLogoutDemo from "@/components/client-logout-demo";
 import UpdateNumberModal from "@/components/update-number-modal";
-import { SiteHeader } from "@/components/sidebar/site-header";
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@workspace/ui/components/sidebar";
-import { AppSidebar } from "@/components/sidebar/app-sidebar";
 
 export default async function HomePage() {
   const user = await prisma.user.findFirst();
@@ -23,34 +17,17 @@ export default async function HomePage() {
         </div>
       ) : (
         <div className="[--header-height:calc(--spacing(14))]">
-          <SidebarProvider className="flex flex-col">
-            <SiteHeader />
-            <div className="flex flex-1">
-              <AppSidebar />
-              <SidebarInset>
-                <div className="flex flex-1 flex-col gap-4 p-4">
-                  <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="bg-muted/50 aspect-video rounded-xl" />
-                    <div className="bg-muted/50 aspect-video rounded-xl" />
-                    <div className="bg-muted/50 aspect-video rounded-xl" />
-                  </div>
-                  <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-                </div>
-              </SidebarInset>
+          <div className="flex flex-col items-center justify-center gap-4 mt-20">
+            <div className="bg-orange-400 p-4 rounded-lg text-black">
+              <p className="font-bold">Name: {user?.name}</p>
+              <p className="font-bold">Email : {user?.email}</p>
+              <p className="font-bold">Ph No : {user?.number}</p>
             </div>
-          </SidebarProvider>
+
+            <ClientLogoutDemo />
+          </div>
         </div>
       )}
     </>
   );
 }
-
-/* <div className="flex flex-col items-center justify-center gap-4 mt-20">
-  <div className="bg-orange-400 p-4 rounded-lg text-black">
-    <p className="font-bold">Name: {user?.name}</p>
-    <p className="font-bold">Email : {user?.email}</p>
-    <p className="font-bold">Ph No : {user?.number}</p>
-  </div>
-
-  <ClientLogoutDemo />
-</div>; */
