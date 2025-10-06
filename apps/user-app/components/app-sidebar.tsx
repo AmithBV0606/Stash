363c36a2459082209428eb4a341e6f1e83d38c85
Amith B V <amithrao0606@gmail.com>
2025-10-06 13:03:56 +0530

    updated "nav-user" sidebar component to show the data from the session.

--- apps/user-app/components/app-sidebar.tsx
+++ apps/user-app/components/app-sidebar.tsx
@@ -29,11 +29,6 @@ import {
 } from "@workspace/ui/components/sidebar";
 
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
   navMain: [
     {
       title: "Home",
@@ -159,7 +154,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         {/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser />
       </SidebarFooter>
     </Sidebar>
   );
--- apps/user-app/components/nav-user.tsx
+++ apps/user-app/components/nav-user.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import {
-  BadgeCheck,
-  Bell,
-  ChevronsUpDown,
-  CreditCard,
-  LogOut,
-  Sparkles,
-} from "lucide-react";
+import { ChevronsUpDown, LogOut } from "lucide-react";
 
 import {
   Avatar,
@@ -17,7 +10,6 @@ import {
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
@@ -31,19 +23,14 @@ import {
 } from "@workspace/ui/components/sidebar";
 import { LogoutModal } from "@/components/auth/logout-modal";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string;
-    email: string;
-    avatar: string;
-  };
-}) {
+export function NavUser() {
   const [logoutOpen, setLogoutOpen] = useState<boolean>(false);
 
   const { isMobile } = useSidebar();
+
+  const { data: session } = useSession();
 
   return (
     <>
@@ -58,13 +45,22 @@ export function NavUser({
                 className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
               >
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarImage
+                    src={session?.user.image!}
+                    alt={session?.user.name}
+                  />
+                  <AvatarFallback className="rounded-lg">
+                    {session?.user.name.slice(0, 2).toUpperCase()}
+                  </AvatarFallback>
                 </Avatar>
 
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-medium">
+                    {session?.user.name}
+                  </span>
+                  <span className="truncate text-xs">
+                    {session?.user.email}
+                  </span>
                 </div>
                 <ChevronsUpDown className="ml-auto size-4" />
               </SidebarMenuButton>
@@ -79,12 +75,21 @@ export function NavUser({
               <DropdownMenuLabel className="p-0 font-normal">
                 <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                   <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src={user.avatar} alt={user.name} />
-                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                    <AvatarImage
+                      src={session?.user.image!}
+                      alt={session?.user.name}
+                    />
+                    <AvatarFallback className="rounded-lg">
+                      {session?.user.name.slice(0, 2)}
+                    </AvatarFallback>
                   </Avatar>
                   <div className="grid flex-1 text-left text-sm leading-tight">
-                    <span className="truncate font-medium">{user.name}</span>
-                    <span className="truncate text-xs">{user.email}</span>
+                    <span className="truncate font-medium">
+                      {session?.user.name}
+                    </span>
+                    <span className="truncate text-xs">
+                      {session?.user.email}
+                    </span>
                   </div>
                 </div>
               </DropdownMenuLabel>
