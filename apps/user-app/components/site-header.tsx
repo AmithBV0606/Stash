f76f23dfca09a64f8b938fb89072ccada7a96c24
Amith B V <amithrao0606@gmail.com>
2025-10-06 19:09:09 +0530

    Removed the search bar from the sidebar's siteheader component.

--- apps/user-app/components/site-header.tsx
+++ apps/user-app/components/site-header.tsx
@@ -2,7 +2,6 @@
 
 import { SidebarIcon } from "lucide-react";
 
-import { SearchForm } from "@/components/search-form";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -36,14 +35,13 @@ export function SiteHeader() {
               <BreadcrumbLink href="#">Stash Dashboard</BreadcrumbLink>
             </BreadcrumbItem>
 
-            {/* <BreadcrumbSeparator /> */}
+            <BreadcrumbSeparator />
 
-            {/* <BreadcrumbItem>
+            <BreadcrumbItem>
               <BreadcrumbPage>Home</BreadcrumbPage>
-            </BreadcrumbItem> */}
+            </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
-        <SearchForm className="w-full sm:ml-auto sm:w-auto" />
       </div>
     </header>
   );
