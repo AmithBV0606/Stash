2066572fae5fff7e381c1925d058ed93ce0c10ee
Amith B V <amithrao0606@gmail.com>
2025-10-19 17:37:02 +0530

    Created an API route for add-money feature, which extracts the bank name and user id from body & makes another request(forwards the request) to the banking server.

---
+++ apps/user-app/app/api/add-money/route.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { user_id, bank_name } = await req.json();
+
+    if (!user_id && !bank_name) {
+      return NextResponse.json({
+        success: false,
+        message: "Bank name not selected.",
+      });
+    }
+
+    const { data } = await axios.post(
+      `http://localhost:3001/bank/${bank_name}`,
+      {
+        user_id,
+        bank_name,
+      }
+    );
+
+    if (!data) {
+      return NextResponse.json({
+        success: false,
+        message: "Token was not generated due to some server error.",
+      });
+    }
+
+    return NextResponse.json({
+      success: true,
+      message: "Successfully forwarded the request to bank servers.",
+      token: data.token,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Failed to forwarded the request to the bank servers.",
+      },
+      { status: 500 }
+    );
+  }
+}
