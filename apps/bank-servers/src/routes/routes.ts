7fc49d8aa42a494f3830175b5c9de9f2de8e0f62
Amith B V <amithrao0606@gmail.com>
2025-10-20 20:27:09 +0530

    Had written "BankController" instead of "PaymentController" in the payments route.

--- apps/bank-servers/src/routes/routes.ts
+++ apps/bank-servers/src/routes/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { BankController } from "../controllers/bankControllers";
+import { PaymentController } from "../controllers/paymentControllers";
 
 const router: Router = Router();
 
 // Bank routes : examples : /bank/hdfc, /bank/axis, etc.
 router.post("/:bank_name", BankController);
+
+// For making the payment :
+router.post("/payment/:bank_name", PaymentController);
 
 export default router;
